1b7d44042c346cb4a892a2892d965f9a83636825
arianrhod <theguidanceofawhitetower@gmail.com>
2015-05-08 00:21:27 +0800

    format code

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ var app = express();
 var logger = require('morgan');
 
 app
-    .set('port', (process.env.PORT || 5000))
-    .use(express['static'](__dirname + '/public'))
-    .use(require('harp').mount(__dirname + '/public'))
-    .set('views', __dirname + '/public')
-    .set('view engine', 'jade')
-    .use(logger('combined'))
-    .use(require('./app/router'))
-    .use(function(req, res) {
-        res.send('404 not found');
-    })
-    .listen(app.get('port'), function() {
-        console.log('Node app is running at localhost:' + app.get('port'));
-    });
+  .set('port', (process.env.PORT || 5000))
+  .use(express['static'](__dirname + '/public'))
+  .use(require('harp').mount(__dirname + '/public'))
+  .set('views', __dirname + '/public')
+  .set('view engine', 'jade')
+  .use(logger('combined'))
+  .use(require('./app/router'))
+  .use(function(req, res) {
+      res.send('404 not found');
+  })
+  .listen(app.get('port'), function() {
+      console.log('Node app is running at localhost:' + app.get('port'));
+  });
