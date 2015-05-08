ec8ffdfd92e6b9f7a332852f591c87fb873f70bc
arianrhod <theguidanceofawhitetower@gmail.com>
2015-05-09 00:04:12 +0800

    add trust proxy

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ var logger = require('morgan')
 
 app
   .set('port', process.env.PORT || 5000)
-  .use(express['static'](__dirname + '/public'))
-  .use(require('harp').mount(__dirname + '/public'))
+  .set('trust proxy', 'loopback')
   .set('views', __dirname + '/public')
   .set('view engine', 'jade')
+  .use(express['static'](__dirname + '/public'))
+  .use(require('harp').mount(__dirname + '/public'))
   .use(logger('combined'))
   .use(require('./app/router'))
   .use(function(req, res) {
