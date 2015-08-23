bd0d193329ec0dbf6bc5e05bd2e962771bf51a4e
Arianrhod <theguidanceofawhitetower@gmail.com>
2015-08-24 01:05:06 +0800

    compress response

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var logger = require('koa-logger')
 var favicon = require('koa-favicon')
 var serve = require('koa-static')
 var json = require('koa-json')
+var fresh = require('koa-fresh')
+var gzip = require('koa-gzip')
 
 var app = koa()
 var router = require('./app/router')
@@ -11,6 +13,8 @@ var router = require('./app/router')
 var port = process.env.PORT || 5000
 
 app
+  .use(gzip())
+  .use(fresh())
   .use(favicon('./public/favicon.ico'))
   .use(views('./view', 'jade'))
   .use(serve('./public'))
