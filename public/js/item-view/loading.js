a4f62b1bc63d229f25a52b579256ed40b9306518
Arianrhod <theguidanceofawhitetower@gmail.com>
2015-09-09 23:32:30 +0800

    add some interfaces to loading ui

--- public/js/item-view/loading.js
+++ public/js/item-view/loading.js
@@ -1,6 +1,19 @@
 const LoadingView = Mn.ItemView.extend({
   className: 'loading',
-  template: '#loading-template'
+  template: '#loading-template',
+  ui: {
+    warn: '.warn',
+    error: '.error'
+  },
+
+  warn() {
+  	this.ui.warn.removeClass('hidden')
+  },
+
+  error() {
+  	this.ui.warn.addClass('hidden')
+  	this.ui.error.removeClass('hidden')
+  }
 })
 
 export default LoadingView
