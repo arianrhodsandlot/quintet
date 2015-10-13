54e78dd1baf284798079e0d3b97804d75956d8a8
Arianrhod <theguidanceofawhitetower@gmail.com>
2015-10-13 21:15:18 +0800

    disable dev response

--- app/controller.js
+++ app/controller.js
@@ -71,7 +71,7 @@ const controller = {
 
     try {
       // for dev use
-      return this.body = require('./utils/search-results2json/scheme')
+      // return this.body = require('./utils/search-results2json/scheme')
 
       const searchResponse = yield request(requestOption)
 
