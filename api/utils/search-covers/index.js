580784d7124233d8d4dab88565cef6b82df919d6
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2017-01-23 16:04:56 +0800

    fix iTunes covers

--- api/utils/search-covers/index.js
+++ api/utils/search-covers/index.js
@@ -5,13 +5,25 @@ const request = (options) => Q.denodeify(require('request'))(options).then(_.fir
 const cheerio = require('cheerio')
 const path = require('path')
 
-const getCoverOriginSrcFromItunes = function (src) {
+const getCertainSizeSrcFromItunes = function (src, size) {
   let parsedSrc = url.parse(src, true)
   let parsedPath = path.parse(parsedSrc.pathname)
-  parsedPath.name = parsedPath.name.replace(/\d{3}x\d{3}/, '1200x1200')
+  parsedPath.name = parsedPath.name.replace(/\d{3,}x\d{3,}/, `${size}x${size}`)
   parsedPath.base = `${parsedPath.name}${parsedPath.ext}`
   parsedSrc.pathname = path.format(parsedPath)
   return url.format(parsedSrc)
+}
+
+const getThumbnailSrc = function (src) {
+  const coverHost = url.parse(src, true).hostname
+  if (coverHost.endsWith('.mzstatic.com')) {
+    return getCertainSizeSrcFromItunes(src, 600)
+  }
+  return src
+}
+
+const getCoverOriginSrcFromItunes = function (src) {
+  return getCertainSizeSrcFromItunes(src, 1200)
 }
 
 const getCoverOriginSrcFrom163 = function (src) {
@@ -26,9 +38,8 @@ const getCoverOriginSrc = function (src) {
     return getCoverOriginSrcFromItunes(src)
   } else if (coverHost.endsWith('.music.126.net')) {
     return getCoverOriginSrcFrom163(src)
-  } else {
-    return src
   }
+  return src
 }
 
 const convertResultHtml2Json = function (resultHtml, site) {
@@ -41,6 +52,7 @@ const convertResultHtml2Json = function (resultHtml, site) {
     title: meta.pt,
     refer: meta.ru,
     src: meta.ou,
+    thumbnailSrc: getThumbnailSrc(meta.ou),
     originSrc: getCoverOriginSrc(meta.ou)
   }
 
@@ -70,9 +82,10 @@ const searchCovers = function (site, query) {
       'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
     }
   }
-  return request(requestOption).then(function (searchResponse) {
-    return searchResults2json(searchResponse.body)
-  })
+  return request(requestOption)
+    .then(function (searchResponse) {
+      return searchResults2json(searchResponse.body)
+    })
 }
 
 module.exports = searchCovers
