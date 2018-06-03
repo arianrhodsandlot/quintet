0500a7f240c19bac1b2c68b17f0ea12cef71bd29
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2018-06-03 16:17:36 +0800

    minor interaction improvements

--- src/public/javascripts/app.js
+++ src/public/javascripts/app.js
@@ -1,95 +1,111 @@
-import '@babel/polyfill'
-import _ from 'lodash'
-import querystring from 'querystring'
-import $ from 'jquery'
-import Cookies from 'cookies-js'
-import imagesLoaded from 'imagesloaded'
-import util from 'util'
+let request
+const $body = $('body')
+const $form = $('form')
+const $query = $form.find('.query')
+const $chips = $('.chips')
+const $albums = $('.albums-result')
+const $loader = $('.album-placeholder')
 
-$(() => {
-  let request
-  const $body = $('body')
-  const $form = $('form')
-  const $query = $form.find('.query')
-  const $chips = $('.chips')
-  const $albums = $('.albums-result')
-  const $loader = $('.album-placeholder')
+function updateBg (src) {
+  if (!src) return
 
-  $query.on('input', _.debounce(function () {
-    $form.submit()
-  }, 500))
+  $('.bg:not(:last)').remove()
 
-  $form.submit(async function (e) {
-    e.preventDefault()
+  const $bg = $('.bg')
+  const $newBg = $('.bg').clone()
+  Cookies('bg', src)
+  $newBg.css({'background-image': `url(${src})`})
+  $bg.before($newBg)
 
-    const query = $query.val().trim()
+  setTimeout(() => {
+    $bg.css({opacity: 0})
+    setTimeout(() => {
+      $bg.remove()
+    }, 3000)
+  }, 100)
+}
 
-    if (!query) {
-      page.replace('/')
-      return
-    }
+$query.on('input', _.debounce(function () {
+  $form.submit()
+}, 500))
 
-    const parsed = querystring.parse($form.serialize())
-    parsed.site = Cookies('site')
-    const url = `${$form.attr('action')}?${$form.serialize()}`
-    page(url)
-  })
+$form.submit(async function (e) {
+  e.preventDefault()
 
-  $chips.on('click', '.mdc-chip', function () {
-    const index = $(this).index()
-    const {chips} = this.parentElement.MDCChipSet
-    const targetChip = chips[index]
+  const query = $query.val().trim()
 
-    chips.forEach((chip) => {
-      chip.foundation.setSelected(chip === targetChip)
-    })
+  if (!query) {
+    page.replace('/')
+    return
+  }
 
-    const site = targetChip.root_.dataset.site
-    Cookies('site', site)
-    const parsed = querystring.parse(location.search.slice(1))
-    parsed.site = site
-    page.replace('/search?' + querystring.stringify(parsed))
-  })
-
-  page(function (ctx, next) {
-    if (ctx.init) {
-      mdc.autoInit(document.body)
-      $query.select()
-    } else {
-      next()
-    }
-  })
-
-  page('/', function (ctx) {
-    $body.attr('class', 'page-index')
-    $query.focus().val('')
-
-    $albums.empty()
-  })
-
-  page('/search', async function (ctx) {
-    $body.attr('class', 'page-search')
-    const {query} = querystring.parse(ctx.querystring)
-    $query.focus().val(query)
-
-    if ($albums.find('.album-container').length) {
-      $albums.addClass('loading')
-    } else {
-      $albums.empty()
-      $loader.show()
-    }
-
-    if (request) request.abort()
-    request = $.get(ctx.path)
-
-    const res = await request
-    const $res = $(res)
-    imagesLoaded($res.get(0), () => {
-      $loader.hide()
-      $albums.html($res.html()).removeClass('loading')
-    })
-  })
-
-  page()
+  const parsed = Qs.parse($form.serialize())
+  parsed.site = Cookies('site')
+  const url = `${$form.attr('action')}?${$form.serialize()}`
+  page(url)
 })
 
+$chips.on('click', '.mdc-chip', function () {
+  const index = $(this).index()
+  const {chips} = this.parentElement.MDCChipSet
+  const targetChip = chips[index]
+
+  chips.forEach((chip) => {
+    chip.foundation.setSelected(chip === targetChip)
+  })
+
+  const site = targetChip.root_.dataset.site
+  Cookies('site', site)
+  const parsed = Qs.parse(location.search.slice(1))
+  parsed.site = site
+  page.replace('/search?' + Qs.stringify(parsed))
+})
+
+$albums.on('click', '.download-mask', function () {
+  const $container = $(this).closest('.album-container')
+  const src = $container.find('img.album-img').attr('src')
+  updateBg(src)
+})
+
+page(function (ctx, next) {
+  if (request) request.abort()
+
+  if (ctx.init) {
+    mdc.autoInit(document.body)
+    $query.select()
+  } else {
+    next()
+  }
+})
+
+page('/', function (ctx) {
+  $body.attr('class', 'page-index')
+  $query.focus().val('')
+
+  $albums.empty()
+})
+
+page('/search', async function (ctx) {
+  $body.attr('class', 'page-search')
+  const {query} = Qs.parse(ctx.querystring)
+  $query.focus().val(query)
+
+  if ($albums.find('.album-container').length) {
+    $albums.addClass('loading')
+  } else {
+    $albums.empty()
+    $loader.show()
+  }
+
+  request = $.get(ctx.path)
+
+  const res = await request
+  const $res = $(res)
+  imagesLoaded($res.get(0), () => {
+    $loader.hide()
+    $albums.html($res.html()).removeClass('loading')
+    updateBg($res.find('.album-img').attr('src'))
+  })
+})
+
+page()
