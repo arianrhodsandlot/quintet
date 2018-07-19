3f797428d4b181c7e15867d00790307f7ea8d8fd
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2018-07-19 23:49:42 +0800

    test deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,10 +16,10 @@ module.exports = {
   deploy : {
     production : {
       host : process.env.HOLLY_QUINTET_HOST,
-      ref  : 'origin/master',
+      ref  : 'origin/dev',
       repo : packageJson.repository.url,
       path : process.env.HOLLY_QUINTET_HOST_DEPLOY_PATH,
-      'post-deploy' : 'npm i && npx pm2 startOrGracefulReload ecosystem.config.js'
+      'post-deploy' : 'touch test'
     }
   }
 };
